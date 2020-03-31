802ea01af1d6cc77721e15dd4b04107597a5606b
tobeck <tobias.becker.olsson@gmail.com>
2020-03-31 22:15:13 +0200

    Initial commit handlers/users.go, NewUsers, marshal json from sample data

---
+++ handlers/users.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"tobeck.github.com/user-api/data"
+	"encoding/json"
+)
+
+// Users struct
+type Users struct {
+	l *log.Logger
+}
+
+// NewUsers func
+func NewUsers(l*log.Logger) *Users {
+	return &Users{l}
+}
+
+// ServeHTTP func
+func (u *Users) ServeHTTP(rw http.ResponseWriter, h *http.Request) {
+	lu :=data.GetUsers()
+	d, err := json.Marshal(lu)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+	rw.Write(d)
+}
