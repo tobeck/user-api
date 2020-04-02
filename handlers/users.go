1098be34480bb8ce9457c542e4e823d9269d1e58
tobeck <tobias.becker.olsson@gmail.com>
2020-04-02 11:35:38 +0200

    Refactored for gorilla/mux, made getUser public, formatting

--- handlers/users.go
+++ handlers/users.go
@@ -3,33 +3,26 @@ package handlers
 import (
 	"log"
 	"net/http"
+
 	"github.com/tobeck/user-api/data"
 )
 
-// Users struct
+// Users logging struct
 type Users struct {
 	l *log.Logger
 }
 
-// NewUsers func to write json
-func NewUsers(l*log.Logger) *Users {
+// NewUsers dependency injection func
+func NewUsers(l *log.Logger) *Users {
 	return &Users{l}
 }
 
-// ServeHTTP
-func (u*Users) ServeHTTP(rw http.ResponseWriter, r*http.Request) {
-	if r.Method == http.MethodGet {
-		u.getUsers(rw, r)
-		return
-	}
+// GetUsers func return Users to the ResponseWriter
+func (u *Users) GetUsers(rw http.ResponseWriter, r *http.Request) {
+	u.l.Println("GET Users")
 
-	// Catch all for not yet implemented
-	rw.WriteHeader(http.StatusMethodNotAllowed)
-}
-
-// getUsers func return Users to the ResponseWriter
-func (u *Users) getUsers(rw http.ResponseWriter, h *http.Request) {
 	lu := data.GetUsers()
+
 	// Use ToJSON encoder instead of marshal, to avoid having to buffer into memory
 	err := lu.ToJSON(rw)
 	if err != nil {
