9292d49f077e9dd5e3ee367ce8f86050ce1322cd
tobeck <tobias.becker.olsson@gmail.com>
2020-04-06 10:47:34 +0200

    Added middleware and AddUser func, upd var ref GetUsers

--- handlers/users.go
+++ handlers/users.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	"log"
 	"net/http"
 
@@ -17,15 +18,41 @@ func NewUsers(l *log.Logger) *Users {
 	return &Users{l}
 }
 
-// GetUsers func return Users to the ResponseWriter
+// GetUsers function to retrieve users
 func (u *Users) GetUsers(rw http.ResponseWriter, r *http.Request) {
 	u.l.Println("GET Users")
+	usr := data.GetUsers()
 
-	lu := data.GetUsers()
-
-	// Use ToJSON encoder instead of marshal, to avoid having to buffer into memory
-	err := lu.ToJSON(rw)
+	err := usr.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+// AddUser function to add new users
+func (u *Users) AddUser(rw http.ResponseWriter, r *http.Request) {
+	u.l.Println("POST Users")
+	usr := r.Context().Value(KeyUser{}).(data.User)
+	data.AddUser(&usr)
+}
+
+// KeyUser struct
+type KeyUser struct {
+}
+
+// MiddlewareValidateUser func
+func (u Users) MiddlewareValidateUser(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		usr := data.User{}
+
+		err := usr.FromJSON(r.Body)
+		if err != nil {
+			http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+			return
+		}
+		ctx := context.WithValue(r.Context(), KeyUser{}, usr)
+		req := r.WithContext(ctx)
+
+		next.ServeHTTP(rw, req)
+	})
 }
